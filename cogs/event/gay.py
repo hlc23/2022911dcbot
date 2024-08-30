ba3c4b770395dda545efaa019517779b688580f9
hlc23 <henryleecode23@gmail.com>
2024-08-30 16:34:09 +0800

    Update gay.py

--- cogs/event/gay.py
+++ cogs/event/gay.py
@@ -2,18 +2,18 @@ import discord
 from core.cog_classes import Cog_base
 
 class GayCog(Cog_base):
-
+    
     @discord.Cog.listener()
     async def on_member_update(self, before: discord.Member, after: discord.Member):
-        if after.id == 417215201423917078 and after.nick != "南同":
-            await after.edit(nick="南同")
+        if after.id == 417215201423917078 and after.nick != "男同":
+            await after.edit(nick="男同")
         return
 
     @discord.Cog.listener()
     async def on_ready(self):
         if gay := self.bot.get_guild(417215201423917078).get_member(417215201423917078):
-            if gay.nick != "南同":
-                await gay.edit(nick="南同")
+            if gay.nick != "男同":
+                await gay.edit(nick="男同")
 
 
 def setup(bot):
