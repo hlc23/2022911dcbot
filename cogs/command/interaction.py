2895ad34546fa603576ef35f106cfa118e2fffc2
hlc23 <henryleecode23@gmail.com>
2024-12-31 16:23:10 +0800

    Update interaction.py

--- cogs/command/interaction.py
+++ cogs/command/interaction.py
@@ -4,13 +4,13 @@ from core.cog_classes import Cog_base
 
 class Interaction(Cog_base):
     
-    @Cog_base.command(name="family_mart", aliases=["fm"])
+    @discord.slash_command(name="family_mart")
     async def family_mart(self, ctx: discord.ApplicationContext):
-        await ctx.respond(f"{choice(["上", "下"])}{randint(1, 5)}{choice(["左", "右"])}{randint(1, 8)}")
+        await ctx.respond(f"{choice(['上', '下'])}{randint(1, 5)}{choice(['左', '右'])}{randint(1, 8)}")
 
-    @Cog_base.command(name="dice", aliases=["roll"])
-    async def dice(self, ctx: discord.ApplicationContext, sides: int = 6):
-        await ctx.respond(f"{randint(1, sides)}")
+    @discord.slash_command(name="dice", description="Roll a dice")
+    async def dice(self, ctx: discord.ApplicationContext):
+        await ctx.respond(f"{randint(1, 6)}")
 
 def setup(bot: discord.Bot):
     bot.add_cog(Interaction(bot))
